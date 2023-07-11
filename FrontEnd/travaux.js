dbb29b1fa946231cca11c85a191465d914844980
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2023-07-11 13:37:27 +0200

    Update travaux.js & index.html & style.css Intégration du fonctionnement des filtres sur la gallerie de travaux
    Signed-off-by: Jean-Baptiste-Paris <ecrireajb@gmail.com>

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -3,8 +3,6 @@ const reponse = await fetch('http://localhost:5678/api/works');
 const travaux = await reponse.json();
 
 function genererTravaux(travaux) {
-    // Récupération de l'élément du DOM qui accueillera les travaux
-    const sectionTravaux = document.querySelector(".gallery");
     for (let i = 0 ; i < travaux.length ; i++) {
 
         // Récupération du travail observé dans ce passage de la boucle
@@ -25,4 +23,53 @@ function genererTravaux(travaux) {
     }
 }
 
+// Récupération de l'élément du DOM qui accueillera les travaux
+const sectionTravaux = document.querySelector(".gallery");
 genererTravaux(travaux);
+
+// Récupération des filtres
+const filtres = document.querySelectorAll(".btn-filtre");
+const filtreTous = document.querySelector("#btn-tous");
+const filtreObjets = document.querySelector("#btn-objets");
+const filtreAppartements = document.querySelector("#btn-appart");
+const filtreHotelsRestos = document.querySelector("#btn-hotels-restos");
+
+// Gestion des filtres
+$(filtreTous).addClass("actif");
+
+filtreTous.addEventListener("click", function () {
+    $(filtres).removeClass("actif");
+    $(filtreTous).addClass("actif");
+    sectionTravaux.innerHTML = "";
+    genererTravaux(travaux);
+});
+
+filtreObjets.addEventListener("click", function () {
+    $(filtres).removeClass("actif");
+    $(filtreObjets).addClass("actif");
+    const travauxFiltres = travaux.filter(function (travaux) {
+        return travaux.category.id == 1
+    });
+    sectionTravaux.innerHTML = "";
+    genererTravaux(travauxFiltres);
+})
+
+filtreAppartements.addEventListener("click", function () {
+    $(filtres).removeClass("actif");
+    $(filtreAppartements).addClass("actif");
+    const travauxFiltres = travaux.filter(function (travaux) {
+        return travaux.category.id == 2
+    });
+    sectionTravaux.innerHTML = "";
+    genererTravaux(travauxFiltres);
+})
+
+filtreHotelsRestos.addEventListener("click", function () {
+    $(filtres).removeClass("actif");
+    $(filtreHotelsRestos).addClass("actif");
+    const travauxFiltres = travaux.filter(function (travaux) {
+        return travaux.category.id == 3
+    });
+    sectionTravaux.innerHTML = "";
+    genererTravaux(travauxFiltres);
+})

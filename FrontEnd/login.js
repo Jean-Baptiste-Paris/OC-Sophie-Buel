2fc25e19a25ab33ed90327585c06759081e7b63b
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2023-07-20 21:03:25 +0200

    Update login.js login.html index.html Gestion de la connexion d'un utilisateur existant et stockage des données reçues
    Signed-off-by: Jean-Baptiste-Paris <ecrireajb@gmail.com>

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -11,6 +11,14 @@ let loggedUser = {
 
 // Requête de connexion sur /users/login et gestion des réponses
 async function attemptLogin(user) {
+    // Vérification de la présence d'un précédent message d'erreur
+    const previousMessage = document.getElementsByClassName('error');
+    if (previousMessage.length > 0) {
+        previousMessage[0].remove();                            // Le délai après avoir éffacé le précedent message permet
+        await new Promise(resolve => setTimeout(resolve, 100)); // un retour visuel indiquant une nouvelle tentative de connexion
+    }
+
+    // Requête
     const response = await fetch(API_URL, {
         method: 'POST',
         headers: {
@@ -20,6 +28,7 @@ async function attemptLogin(user) {
         body: JSON.stringify(user),
     });
 
+    // Gestion de la réponse
     switch (response.status) {
         case 401: // erreur d'autorisation
         case 404: // utilisateur non trouvé
@@ -28,18 +37,14 @@ async function attemptLogin(user) {
         case 200: // utilisateur valide, reception de l'element loggedUser
             loggedUser = await response.json();
             console.log(loggedUser);
+            sessionStorage.setItem('userId', loggedUser.userId);
+            sessionStorage.setItem('userToken', loggedUser.token);
+            window.location.href = '/index.html';
             break;
     }
 }
 
 async function incorrectUserMessage() {
-    // Vérification de la présence d'un précédent message d'erreur
-    const previousMessage = document.getElementsByClassName('error');
-    if (previousMessage.length > 0) {
-        previousMessage[0].remove();                            // Le délai après avoir éffacé le précedent message permet
-        await new Promise(resolve => setTimeout(resolve, 100)); // un retour visuel indiquant une nouvelle tentative de connexion
-    }
-
     // Création d'un nouveau message d'erreur qui s'affiche en haut du formulaire
     const message = document.createElement('p');
     message.innerText = 'Erreur dans l’identifiant ou le mot de passe';

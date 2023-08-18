46526ffcee3fe0fe90894753fb3c4eeb43876ec0
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2023-08-18 20:28:45 +0200

    Update modals-display.js and modals-modal1.js Affichage dynamique du contenu spécifique à la modale 1
    Signed-off-by: Jean-Baptiste-Paris <ecrireajb@gmail.com>

--- FrontEnd/modals-display.js
+++ FrontEnd/modals-display.js
@@ -2,12 +2,13 @@
 import { createModalElement } from "./modals-create.js";
 import { modalsData } from "./config.js";
 import { mainTag } from "./dom-elements.js";
+import { modal1CreateContent } from "./modals-modal1.js";
 
 function openModal(modalId){
     const modal = createModalElement(modalId, modalsData[0].title);
-    // modal1CreateContent(modal);
-    // mainTag.appendChild(modal);
     mainTag.appendChild(modal);
+    const contentWrapper = modal.querySelector('.modal-content');
+    modal1CreateContent(contentWrapper);
 }
 
 export { openModal }
--- FrontEnd/modals-modal1.js
+++ FrontEnd/modals-modal1.js
@@ -1,14 +1,26 @@
 //modals-modal1.js
+import { fetchWorks } from "./works-api.js";
 import { createWorkCards } from "./works-cards.js";
 
-function modal1CreateContent(modalElement) {
+async function modal1CreateContent(modalContentWrapper) {
     const galleryElement = document.createElement('div');
     galleryElement.classList.add('gallery', 'modal-gallery');
 
+    const currentWorks = await fetchWorks();
     createWorkCards(currentWorks, galleryElement);
 
-    const modalH3 = modalElement.querySelector('h3');
-    modalElement.insertAfter()
+    const buttonElement = document.createElement('button');
+    buttonElement.id = "btn-add-work";
+    buttonElement.href = "#modal2";
+    buttonElement.innerText = "Ajouter une photo";
+
+    const linkElement = document.createElement('a');
+    linkElement.href = '#';
+    linkElement.innerText = "Supprimer la galerie";
+
+    modalContentWrapper.appendChild(galleryElement);
+    modalContentWrapper.appendChild(buttonElement);
+    modalContentWrapper.appendChild(linkElement);
 }
 
 export { modal1CreateContent }
